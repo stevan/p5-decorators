a908a58010ee8a385bfaca070db73aa14d1ef344
Stevan Little <stevan.little@gmail.com>
2017-08-14 19:56:12 +0200

    version bump

--- lib/Method/Traits/Meta/Provider.pm
+++ lib/Method/Traits/Meta/Provider.pm
@@ -4,7 +4,7 @@ package Method::Traits::Meta::Provider;
 use strict;
 use warnings;
 
-our $VERSION   = '0.04';
+our $VERSION   = '0.05';
 our $AUTHORITY = 'cpan:STEVAN';
 
 sub OverwritesMethod { () }
