95bd5043cb68e4fe0d34fc05e7d5f6c8ed85073d
Stevan Little <stevan.little@gmail.com>
2017-10-25 22:13:36 +0200

    fixing issue that came up in CPAN testers, so cutting a release

--- lib/Method/Traits/Meta/Provider.pm
+++ lib/Method/Traits/Meta/Provider.pm
@@ -4,7 +4,7 @@ package Method::Traits::Meta::Provider;
 use strict;
 use warnings;
 
-our $VERSION   = '0.06';
+our $VERSION   = '0.07';
 our $AUTHORITY = 'cpan:STEVAN';
 
 sub OverwritesMethod { () }
